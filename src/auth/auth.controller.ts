bd47f67d0bef6b330ae0ae4cf1298b3fbcb172b6
reinaldoper <reinaldoper83@gmail.com>
2025-12-09 15:12:14 -0400

    feat: Cria o controller do modulo auth.

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,66 @@
+import { Body, Controller, Post, Res, HttpCode } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import express from 'express';
+import { UsersService } from 'src/users/users.service';
+
+@ApiTags('Auth')
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
+
+  @Post('login')
+  @HttpCode(200)
+  @ApiOperation({ summary: 'Login' })
+  @ApiResponse({ status: 200, description: 'Login successful' })
+  @ApiResponse({ status: 401, description: 'Invalid credentials' })
+  @ApiResponse({ status: 404, description: 'User not found' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
+  async login(
+    @Body() loginDto: LoginDto,
+    @Res({ passthrough: true }) res: express.Response,
+  ) {
+    const user = await this.authService.validateUser(loginDto);
+
+    if (user) {
+      const token = this.authService.generateToken(user);
+
+      res.cookie('access_token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        maxAge: 1000 * 60 * 60 * 24,
+      });
+
+      return {
+        message: 'Login successful',
+        access_token: token,
+      };
+    }
+    return null;
+  }
+
+  @Post('logout')
+  @HttpCode(204)
+  @ApiOperation({ summary: 'Logout' })
+  @ApiResponse({ status: 204, description: 'User logged out' })
+  logout(@Res({ passthrough: true }) res: express.Response) {
+    res.clearCookie('access_token');
+    return;
+  }
+
+  @Post('register')
+  @HttpCode(201)
+  @ApiOperation({ summary: 'Register new user' })
+  @ApiResponse({ status: 201, description: 'User registered successfully' })
+  @ApiResponse({ status: 409, description: 'User already exists' })
+  @ApiResponse({ status: 400, description: 'Invalid payload' })
+  async register(@Body() dto: CreateUserDto) {
+    return this.usersService.createUser(dto);
+  }
+}
