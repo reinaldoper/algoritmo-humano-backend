4226489a8250d09429d5a5c426378149b5ddea70
reinaldoper <reinaldoper83@gmail.com>
2025-12-09 15:13:28 -0400

    feat: Cria o service com as regras de negócio para o moduo auth.

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,51 @@
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(dto: LoginDto) {
+    const { email, password } = dto;
+    try {
+      const user = await this.prisma.user.findUnique({ where: { email } });
+      if (!user) {
+        throw new NotFoundException('User not found');
+      }
+      const passwordMatches =
+        user && (await bcrypt.compare(password, user.password));
+      if (!passwordMatches) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+      if (user && passwordMatches) {
+        return user;
+      }
+      return null;
+    } catch (error) {
+      if (
+        error instanceof NotFoundException ||
+        error instanceof UnauthorizedException
+      ) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Internal server error');
+    }
+  }
+
+  generateToken(user: User) {
+    const payload = { userId: user.id, email: user.email };
+    return this.jwtService.sign(payload);
+  }
+}
